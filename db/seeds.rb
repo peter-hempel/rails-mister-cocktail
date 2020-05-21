3f3da86cda910ee47b62fc6e9274202366a63dc5
Peter Hempel <hempel.pe@gmail.com>
2020-05-21 16:36:50 +0200

    cocktail recipes done - continue with doses tomorrow

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "some ingredients for cocktails"
+Ingredient.destroy_all
+Cocktail.destroy_all
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "tonic")
+Ingredient.create(name: "cucumber")
+Ingredient.create(name: "coke")
+Ingredient.create(name: "lemon")
+
+Cocktail.create(name: "gin tonic")
+Cocktail.create(name: "cuba libre")
+Cocktail.create(name: "mojito")
+Cocktail.create(name: "moscow mule")
+Cocktail.create(name: "long island ice tea")
+
+puts "Succesfully created!"
